cc1d6568290890f3227b724cc5892d13a808c742
Windokk <68743762+Windokk@users.noreply.github.com>
2021-02-07 20:42:42 +0100

    Add files via upload

---
+++ nervon-pages/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html><head>
+    <link rel="stylesheet" href="/style.css">
+</head>
+    
+
+<header>
+    
+    <div  class="header1">
+		
+			<img src="windokk.png"style="margin-left:30px;margin-top:15px; height:50px;width:50px;"onclick="window.location.href='https://windokkstudio.net';">
+		
+	
+		<button class="button2" onclick="window.location.href='https://windokkstudio.net';"id="Home_Button">Home</button>
+	
+        <button class="button3" onclick="window.location.href='https://windokkstudio.net/games.php';"id="Game_Button">Games</button>
+		
+		<button class="button4" onclick="window.location.href='https://windokkstudio.net/freelance.php';"id="Freelance_Button">Freelance</button>
+		
+		<button class="button5" onclick="window.location.href='https://windokkstudio.net/portfolio.php';"id="Portfolio_Button">Portfolio</button>
+		
+		<div class="search1">
+			<input class="input1" placeholder="Search..." type=text>
+	        <img class="loupe1"src='https://windokkstudio.net/loupe.png' width=30 height=30>
+		</div>
+		
+		
+	</div>    
+    
+    
+       
+    
+</header>
+
+
+</html>
